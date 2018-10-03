2618c3f77c9a0edd1cb8522f8269809b735c3d72
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2018-10-04 01:30:07 +0700

    simple app

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, Response
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    return "Hello World!"
+
+
+@app.route('/<object_name>/area/<int:length>/<int:width>/')
+def area(object_name, length, width):
+    area = length * width
+    response_area = "Area of {} with length {} and width {} is: {}".format(
+        object_name, length, width, area)
+    return Response(response_area)

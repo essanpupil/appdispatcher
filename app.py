0b89b81741d0bb49b527c4c7a9b42ae1b74e5769
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2018-10-04 01:39:34 +0700

    simply customize

--- app.py
+++ app.py
@@ -10,7 +10,10 @@ def home():
 
 @app.route('/<object_name>/area/<int:length>/<int:width>/')
 def area(object_name, length, width):
-    area = length * width
+    if object_name == 'triangle':
+        area = 0.5 * length * width
+    else:
+        area = length * width
     response_area = "Area of {} with length {} and width {} is: {}".format(
         object_name, length, width, area)
     return Response(response_area)

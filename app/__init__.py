b6f5176b4988e5e19270ffa139519b1c586824d2
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2018-10-05 08:52:16 +0700

    production settings

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,20 @@
 from flask import Flask, Response
 
-app = Flask(__name__)
+
+def create_app(config_filename):
+    app = Flask(__name__)
+    app.config.from_pyfile(config_filename)
+
+    app.add_url_rule('/', 'home', home)
+    app.add_url_rule('/<object_name>/area/<int:length>/<int:width>/', 'area', area)
+
+    return app
 
 
-@app.route('/')
 def home():
     return "Hello World!"
 
 
-@app.route('/<object_name>/area/<int:length>/<int:width>/')
 def area(object_name, length, width):
     if object_name == 'triangle':
         area = 0.5 * length * width

f4a5f31ca3ea318e89c05dce8f2333a54be521b0
Ikhsan Noor Rosyidin <jakethitam1985@gmail.com>
2018-10-06 05:31:24 +0700

    path dispatcher

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,8 @@
 from flask import Flask, Response, render_template
 
 
-def create_app(template_dir):
-    app = Flask(__name__, template_folder=template_dir)
+def create_app(prefix):
+    app = Flask(__name__, template_folder='templates_' + prefix)
 
     app.add_url_rule('/', 'home', home)
     app.add_url_rule('/<object_name>/area/<int:length>/<int:width>/', 'area', area)
@@ -10,7 +10,7 @@ def create_app(template_dir):
     return app
 
 
-def home():
+def home(object_name=None):
     return render_template('index.html')
 
 
@@ -22,3 +22,14 @@ def area(object_name, length, width):
     response_area = "Area of {} with length {} and width {} is: {}".format(
         object_name, length, width, area)
     return Response(response_area)
+
+
+def default_app():
+    app = Flask(__name__, template_folder='templates_geometry')
+
+    app.add_url_rule('/<object_name>/', 'home', home)
+    app.add_url_rule('/<object_name>/area/<int:length>/<int:width>/', 'area', area)
+
+    return app
+
+this_app = default_app()
--- wsgi.py
+++ wsgi.py
@@ -1,38 +1,36 @@
-from app import create_app
+from app import create_app, this_app
 from threading import Lock
-from werkzeug.exceptions import NotFound
+from werkzeug.wsgi import pop_path_info, peek_path_info
 
 
-class SubdomainDispatcher(object):
-
-    def __init__(self, domain, create_app):
-        self.domain = domain
+class PathDispatcher():
+    def __init__(self, default_app, create_app):
+        self.default_app = default_app
         self.create_app = create_app
         self.lock = Lock()
         self.instances = {}
 
-    def get_application(self, host):
-        host = host.split(':')[0]
-        assert host.endswith(self.domain), 'Configuration error'
-        subdomain = host[:-len(self.domain)].rstrip('.')
+    def get_application(self, prefix):
         with self.lock:
-            app = self.instances.get(subdomain)
-            if app is None:
-                app = self.create_app(subdomain)
-                self.instances[subdomain] = app
-            return app
+            my_app = self.instances.get(prefix)
+            if my_app is None:
+                my_app = self.create_app(prefix)
+                if my_app is not None:
+                    self.instances[prefix] = my_app
+            return my_app
 
     def __call__(self, environ, start_response):
-        app = self.get_application(environ['HTTP_HOST'])
+        app = self.get_application(peek_path_info(environ))
+        if app is not None:
+            pop_path_info(environ)
+        else:
+            app = self.default_app
         return app(environ, start_response)
 
 
-def make_app(subdomain):
-    if subdomain not in ['circle', 'rectangle', 'triangle']:
-        return NotFound()
-
-    # otherwise create the application for the specific template
-    return create_app('templates_{}'.format(subdomain))
+def make_app(prefix):
+    if prefix in ['rectangle', 'circle', 'triangle']:
+        return create_app(prefix)
 
 
-application = SubdomainDispatcher('geometry.local', make_app)
+application = PathDispatcher(this_app, make_app)
